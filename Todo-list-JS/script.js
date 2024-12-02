cd113647b2d00960c41cf56f3afb9b3497501908
Jiya Arya <jiyaarya1205@gmail.com>
2024-12-02 20:36:23 +0530

    half logic is done

--- Todo-list-JS/script.js
+++ Todo-list-JS/script.js
@@ -1,4 +1,20 @@
-function checkBox(item){
-   item.classList.toggle('checked')
-   
+const listBtn = document.querySelectorAll(".list button");
+const inputItem = document.getElementById("input-item");
+
+function addItem() {
+  let inputItems = document.getElementById("input-item").value;
+  console.log(inputItems);
+  inputItems.value = "";
+}
+
+for (let i = 0; i <= listBtn.length; i++) {
+  listBtn[i].addEventListener("click", function () {
+    console.log(`clicked ${i}`);
+    let parentLi = this.parentElement;
+    parentLi.style.display = "none";
+  });
+}
+
+function checkBox(item) {
+  item.classList.toggle("checked");
 }
